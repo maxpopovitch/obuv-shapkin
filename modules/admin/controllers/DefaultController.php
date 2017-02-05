f90314a04410e51521955fb6e30c04c39687c11a
maxdonetsk <max.donetsk@mail.ru>
2017-02-05 19:44:10 +0200

    login admin

--- modules/admin/controllers/DefaultController.php
+++ modules/admin/controllers/DefaultController.php
@@ -4,19 +4,23 @@ namespace app\modules\admin\controllers;
 
 use Yii;
 use yii\web\Controller;
+use app\models\forms\LoginForm;
 
 /**
  * Default controller for the `admin` module
  */
-class DefaultController extends Controller
-{
+class DefaultController extends Controller {
+
     /**
      * Renders the index view for the module
      * @return string
      */
-    public function actionIndex()
-    {
+    public function actionIndex() {
+        $model = new LoginForm();
+        
         $this->view->params['header'] = '<a href="/">' . Yii::$app->name . '</a>' . ' \ Администрирование';
-        return $this->render('index');
+        return $this->render('index', [
+                    'model' => $model,
+        ]);
     }
 }
--- modules/admin/views/default/index.php
+++ modules/admin/views/default/index.php
@@ -1,13 +1,61 @@
-<div class="admin-default-index">
-    <!--<h1><?= $this->context->action->uniqueId ?></h1>-->
-    <a class="btn btn-primary" href="index.php?r=admin/brand">Торговые марки</a>
-    <p>
-        This is the view content for action "<?= $this->context->action->id ?>".
-        The action belongs to the controller "<?= get_class($this->context) ?>"
-        in the "<?= $this->context->module->id ?>" module.
-    </p>
-    <p>
-        You may customize this page by editing the following file:<br>
-        <code><?= __FILE__ ?></code>
-    </p>
-</div>
+<?php
+/* @var $this yii\web\View */
+/* @var $form yii\bootstrap\ActiveForm */
+/* @var $model app\models\LoginForm */
+
+use yii\helpers\Html;
+use yii\bootstrap\ActiveForm;
+?>
+
+<?php if (Yii::$app->user->isGuest) { ?>
+    <div class="site-login">
+        <?php
+        $form = ActiveForm::begin([
+                    'id' => 'login-form',
+                    'layout' => 'horizontal',
+                    'fieldConfig' => [
+                        'template' => "{label}\n<div class=\"col-lg-3\">{input}</div>\n<div class=\"col-lg-7\">{error}</div>",
+                        'labelOptions' => ['class' => 'col-lg-2 control-label'],
+                    ],
+        ]);
+        ?>
+
+        <?= $form->field($model, 'username')->textInput(['autofocus' => true]) ?>
+
+        <?= $form->field($model, 'password')->passwordInput() ?>
+
+        <?=
+        $form->field($model, 'rememberMe')->checkbox([
+            'template' => "<div class=\"col-lg-offset-2 col-lg-3\">{input} {label}</div>\n<div class=\"col-lg-7\">{error}</div>",
+        ])
+        ?>
+
+        <div class="form-group">
+            <div class="col-lg-offset-2 col-lg-10">
+                <?= Html::submitButton('Login', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
+            </div>
+        </div>
+
+        <?php ActiveForm::end(); ?>
+
+        <div class="col-lg-offset-2" style="color:#999;">
+            You may login with <strong>admin/admin</strong> or <strong>demo/demo</strong>.<br>
+            To modify the username/password, please check out the code <code>app\models\User::$users</code>.
+        </div>
+    </div>
+<?php } ?>
+<?php if (!Yii::$app->user->isGuest) { ?>
+    <div class="admin-default-index">
+        <!--<h1><?= $this->context->action->uniqueId ?></h1>-->
+        <a class="btn btn-primary" href="index.php?r=admin/brand">Торговые марки</a>
+        <p>
+            This is the view content for action "<?= $this->context->action->id ?>".
+            The action belongs to the controller "<?= get_class($this->context) ?>"
+            in the "<?= $this->context->module->id ?>" module.
+        </p>
+        <p>
+            You may customize this page by editing the following file:<br>
+            <code><?= __FILE__ ?></code>
+        </p>
+    </div>
+<?php } ?>
