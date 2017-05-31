3d86465315c17706154ef0b0fb43150597ccf074
maxdonetsk <max.donetsk@mail.ru>
2017-05-31 21:11:12 +0300

    yandex.metrika counter

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -159,7 +159,52 @@ AppAsset::register($this);
 	  </div>
 	  <div class="col-xs-12">
 	    <div class="oc-counter">
-	      <a href="#"><img src="imgs/ymetrika.png" width="88" height="31" alt="Яндекс.Метрика"/></a>
+
+	      <!-- Yandex.Metrika informer -->
+	      <a href="http://metrika.yandex.ru/stat/?id=23142487&amp;from=informer"
+		 target="_blank" rel="nofollow">
+		<img src="//bs.yandex.ru/informer/23142487/3_0_FFFFFFFF_FFFFFFFF_0_pageviews"
+		     style="margin-top:10px; width:88px; height:31px; border:0;" alt="Яндекс.Метрика" title="Яндекс.Метрика: данные за сегодня (просмотры, визиты и уникальные посетители)" onclick="try {
+			   Ya.Metrika.informer({i: this, id: 23142487, lang: 'ru'});
+			   return false
+			 } catch (e) {
+			 }"/>
+	      </a>
+	      <!-- /Yandex.Metrika informer -->
+
+	      <!-- Yandex.Metrika counter -->
+	      <script type="text/javascript">
+		(function (d, w, c) {
+		  (w[c] = w[c] || []).push(function () {
+		    try {
+		      w.yaCounter23142487 = new Ya.Metrika({id: 23142487,
+			webvisor: true,
+			clickmap: true,
+			trackLinks: true,
+			accurateTrackBounce: true});
+		    } catch (e) {
+		    }
+		  });
+
+		  var n = d.getElementsByTagName("script")[0],
+			  s = d.createElement("script"),
+			  f = function () {
+			    n.parentNode.insertBefore(s, n);
+			  };
+		  s.type = "text/javascript";
+		  s.async = true;
+		  s.src = (d.location.protocol == "https:" ? "https:" : "http:") + "//mc.yandex.ru/metrika/watch.js";
+
+		  if (w.opera == "[object Opera]") {
+		    d.addEventListener("DOMContentLoaded", f, false);
+		  } else {
+		    f();
+		  }
+		})(document, window, "yandex_metrika_callbacks");
+	      </script>
+	      <noscript><div><img src="//mc.yandex.ru/watch/23142487" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+	      <!-- /Yandex.Metrika counter -->
+
 	    </div>
 	  </div>
 	  <div class="col-xs-12">
