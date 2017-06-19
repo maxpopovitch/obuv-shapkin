298716839eb2db9ef116cbefafbeb8f09dec7603
maxdonetsk <max.donetsk@mail.ru>
2017-06-19 21:26:48 +0300

    wideness views changed

--- modules/admin/views/wideness/update.php
+++ modules/admin/views/wideness/update.php
@@ -10,12 +10,26 @@ $this->params['breadcrumbs'][] = ['label' => 'Widenesses', 'url' => ['index']];
 $this->params['breadcrumbs'][] = ['label' => $model->name, 'url' => ['view', 'id' => $model->id]];
 $this->params['breadcrumbs'][] = 'Update';
 ?>
-<div class="wideness-update">
+<section class="oc-content">
+  <div class="container-fluid oc-margin">
+    <div class="row row-offcanvas row-offcanvas-right">
+      <div class="col-xs-12">
 
-    <h1><?= Html::encode($this->title) ?></h1>
+	<div class="row">
+	  <div class="col-xs-12">
+	    <div class="oc-ware-div">
+	      <h1><?= Html::encode($this->title) ?></h1>
 
-    <?= $this->render('_form', [
-        'model' => $model,
-    ]) ?>
+	      <?=
+	      $this->render('_form', [
+		  'model' => $model,
+	      ])
+	      ?>
+	    </div>
+	  </div>
+	</div>
 
-</div>
+      </div>
+    </div>
+  </div>
+</section>
